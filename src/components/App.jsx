469a87b0df7b51ec4d386e6e86d2e6de9f9587e9
Avijit Nanda <avijitnanda14@gmail.com>
2023-12-20 00:51:57 +0530

    able to use shadcn and tailwind

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,47 @@
+import * as React from "react"
+
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 function App() {
 
   return (
     <>
-      <div>
-      <Button>Click me</Button>
-      </div>
+      <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Question 1</CardTitle>
+        <CardDescription>Which form looks better?</CardDescription>
+      </CardHeader>
+      <CardContent className="flex justify-around">
+        <Button>Card1</Button>
+        <Button>Card2</Button>
+
+        
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+      
+      
     </>
   )
 }
